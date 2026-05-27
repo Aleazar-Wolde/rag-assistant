6febc574cbc5be6800461dafb0fce76e069d1faf
Aleazar Wolde <aleazarali123@gmail.com>
2026-05-26 21:41:57 -0400

    add load_pdf function to ingest.py

---
+++ ingest.py
@@ -0,0 +1,20 @@
+#Describtion : This is a file which is used to load a document into the chromadb database. It uses the chromadb library to create a client and a collection, and then adds documents to the collection with associated metadata and ids.
+import chromadb
+from langchain_community.document_loaders import PyPDFLoader
+
+# 1. Load the PDF
+def read_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    pages = loader.load()
+    return pages
+
+# 2. split the PDF into chunks
+def split_into_chunks(text):
+    pass
+
+# 3. convert the chunks into vectors
+def convert_to_vectors(chunks):
+    pass
+# 4. store the vectors in the chromadb database
+def store_in_chromadb(vectors):
+    pass
